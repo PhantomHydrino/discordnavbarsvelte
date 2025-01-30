667af3f5d15fa6d86bb914ea8f79303257d2210d
Sattyam Mane <sattyammane@Sattyams-Mac-mini.local>
2025-01-30 12:39:41 +0530

    build
    NOW

---
+++ .netlify/server/nodes/2.js
@@ -0,0 +1,8 @@
+
+
+export const index = 2;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/2.DFSEmX0D.js","_app/immutable/chunks/BNVMqHwu.js","_app/immutable/chunks/WqQjzbQs.js","_app/immutable/chunks/xnbKd7S1.js","_app/immutable/chunks/Cj_OqhnG.js","_app/immutable/chunks/CqSt25Pw.js","_app/immutable/chunks/BkSvtNpJ.js"];
+export const stylesheets = ["_app/immutable/assets/2.Do4RebdY.css"];
+export const fonts = [];
