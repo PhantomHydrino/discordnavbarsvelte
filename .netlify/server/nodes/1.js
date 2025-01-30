601a31248a077c651453ce037c529671e9a4af12
Sattyam Mane <55507011+PhantomHydrino@users.noreply.github.com>
2025-01-30 12:47:29 +0530

    Delete .netlify directory

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BeIU-Wb3.js","_app/immutable/chunks/BNVMqHwu.js","_app/immutable/chunks/WqQjzbQs.js","_app/immutable/chunks/xnbKd7S1.js","_app/immutable/chunks/CqSt25Pw.js","_app/immutable/chunks/iaLqY4F0.js","_app/immutable/chunks/BkSvtNpJ.js"];
-export const stylesheets = [];
-export const fonts = [];
