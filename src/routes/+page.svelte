6b139b04d733ef35440ead78cb4dbbda2cd0c449
Sattyam Mane <sattyammane@Sattyams-Mac-mini.local>
2025-01-30 14:12:36 +0530

    needed changes recognised

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
-<!-- <script>
+<script>
+// @ts-nocheck
+
     import MainScreen from '@components/mainScreen/MainScreen.svelte';
     import SideBar from '@components/sidebar/SideBar.svelte';
     import "../app.css";
-</script> -->
+</script>
 
 <div class="page-container">
     yo
 </div>
 
-<!-- <style>
+<style>
     .page-container {
       display: flex;  /* Flexbox to align the MainScreen and SideBar */
     }
-</style> -->
+</style>
