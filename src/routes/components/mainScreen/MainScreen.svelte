361fb55359fc202d3bf21b2f502a764a49c56900
Sattyam Mane <sattyammane@Sattyams-Mac-mini.local>
2025-01-31 15:01:58 +0530

    fix: UI for small devices
    UI for small devices fixed
    the animated logo only shows up on big screen devices and not the small
    on
    also added discord logo for the background

--- src/routes/components/mainScreen/MainScreen.svelte
+++ src/routes/components/mainScreen/MainScreen.svelte
@@ -2,6 +2,7 @@
     import Icon from "@iconify/svelte";
 </script>
 <div class="flex h-[100vh] w-[100vw] bg-gray-800 justify-center items-center">
-    <Icon icon = {"eos-icons:atom-electron"} class="text-green-400 fixed top-[-20%] right-[-20%] text-[125vh] opacity-[10%] animate-[spin_10s_ease-in-out_infinite]"/>
+    <Icon icon = {"simple-icons:discord"} class="text-gray-950 text-[30vh] drop-shadow-lg"/>
+    <Icon icon = {"eos-icons:atom-electron"} class="text-teal-950 fixed top-[-46vh] right-[-23vw] text-[115vh] animate-[spin_10s_ease-in-out_infinite] invisible lg:visible"/>
 </div>
 
